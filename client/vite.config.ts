a789312c3942c5e21bc1ee89fbbac0cf1098ed16
ByteNinjaSmit <smitrajbankar11@gmail.com>
2024-11-25 13:47:16 +0530

    clinet side tsx to jsx

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'], // Ensure .jsx is in the list of extensions
+  },
   plugins: [react()],
 })
